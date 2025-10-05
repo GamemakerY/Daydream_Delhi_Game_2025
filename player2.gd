d331df62ab2956554ac040b6c984727f568e6c80
Yusuf Amin <Yusuf.amin2307@gmail.com>
2025-10-05 14:58:21 +0530

    Mechanism of both players done

--- player2.gd
+++ player2.gd
@@ -12,18 +12,18 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+	if Input.is_action_just_pressed("ui_w") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("ui_a"):
 		$AnimatedSprite2D.flip_h = true
 		$AnimatedSprite2D.play("Run")
 		
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("ui_d"):
 		$AnimatedSprite2D.flip_h = false
 		$AnimatedSprite2D.play("Run")
 	
-	if Input.is_action_just_pressed("ui_shift"):
+	if Input.is_action_just_pressed("ui_q"):
 		$AnimatedSprite2D.play("Attack")
 	
 	if not $AnimatedSprite2D.animation == 'Attack':
@@ -32,7 +32,7 @@ func _physics_process(delta: float) -> void:
 		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("ui_a", "ui_d")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
