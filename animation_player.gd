db0b41c69d5983f8417258740ac866432de2f849
anasjamshed02-alt <anasjamshed02@gmail.com>
2025-10-05 16:20:36 +0530

    Assets added for cutscenes

---
+++ animation_player.gd
@@ -0,0 +1,28 @@
+extends AnimationPlayer
+extends Label
+
+@export var full_text := "Hello, this is animated text!"
+@export var delay := 0.05  # Delay between letters (seconds)
+var char_index := 0
+
+func _ready():
+	text = ""
+	char_index = 0
+	start_typing()
+
+func start_typing():
+	text = ""
+	char_index = 0
+	var timer = Timer.new()
+	timer.wait_time = delay
+	timer.one_shot = false
+	timer.connect("timeout", Callable(self, "_on_timer_timeout"))
+	add_child(timer)
+	timer.start()
+
+func _on_timer_timeout():
+	if char_index < full_text.length():
+		text += full_text[char_index]
+		char_index += 1
+	else:
+		$Timer.queue_free()  # Stop typing
