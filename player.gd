d20c340f819a265f2199892f113b114a05522930
Yusuf Amin <Yusuf.amin2307@gmail.com>
2025-10-05 12:16:56 +0530

    Coded mechanics for player

--- player.gd
+++ player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -300.0
 
 
 func _physics_process(delta: float) -> void:
@@ -11,9 +11,23 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
+	if Input.is_action_pressed("ui_left"):
+		$AnimatedSprite2D.flip_h = true
+		$AnimatedSprite2D.play("Run")
+		
+	if Input.is_action_pressed("ui_right"):
+		$AnimatedSprite2D.flip_h = false
+		$AnimatedSprite2D.play("Run")
+	
+	if Input.is_action_pressed("ui_shift"):
+		$AnimatedSprite2D.play("Attack")
+	
+	if !Input.is_anything_pressed():
+		$AnimatedSprite2D.play("Idle")
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
